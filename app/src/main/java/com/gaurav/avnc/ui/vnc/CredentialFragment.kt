ffa237743fe7e96f4bc377e7f2e8b37fb94975d7
Gaurav Ujjwal <gujjwal00@gmail.com>
2021-03-22 19:13:34 +0530

    Obfuscate password auto-complete list

--- app/src/main/java/com/gaurav/avnc/ui/vnc/CredentialFragment.kt
+++ app/src/main/java/com/gaurav/avnc/ui/vnc/CredentialFragment.kt
@@ -9,8 +9,12 @@
 package com.gaurav.avnc.ui.vnc
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.gaurav.avnc.R
@@ -76,10 +80,29 @@ class CredentialFragment : DialogFragment() {
             val passwords = list.map { it.password }.filter { it.isNotBlank() }.distinct()
 
             val usernameAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, usernames)
-            val passwordAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, passwords)
+            val passwordAdapter = PasswordAdapter(requireContext(), android.R.layout.simple_list_item_1, passwords)
 
             binding.username.setAdapter(usernameAdapter)
             binding.password.setAdapter(passwordAdapter)
+        }
+    }
+
+    /**
+     * Specialized adapter for password completion. We don't want to show plaintext password
+     * in completion list so we partially obfuscate it.
+     *
+     * @param layout Must point to a resource with [TextView] as root
+     */
+    private class PasswordAdapter(context: Context, layout: Int, passwords: List<String>)
+        : ArrayAdapter<String>(context, layout, passwords) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent) as TextView
+
+            val obfuscated = "******" + view.text.takeLast(2)
+            view.text = obfuscated
+
+            return view
         }
     }
 }
