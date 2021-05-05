fdae547952e1ccb01b392a98db21c3faf404663b
Gaurav Ujjwal <gujjwal00@gmail.com>
2021-05-05 10:18:05 +0530

    Update dependencies

--- app/src/main/java/com/gaurav/avnc/model/ServerProfile.kt
+++ app/src/main/java/com/gaurav/avnc/model/ServerProfile.kt
@@ -11,7 +11,7 @@ package com.gaurav.avnc.model
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 /**
