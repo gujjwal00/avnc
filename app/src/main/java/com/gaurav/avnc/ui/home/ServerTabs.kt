gujjwal00/avnc

commit 2b64933fcb65ceb375fce46695b255b9005430b1

Refactor Discovery
Instead of having different (and confusing) timeouts/restart settings, we now have
a single 'Autorun' mode. When enabled, we keep discovering servers as long as
HomeActivity is in foreground, and automatically stop in background.
So discovered servers won't become stale after timeout.

app/src/main/java/com/gaurav/avnc/ui/home/ServerTabs.kt
@@ -10,8 +10,6 @@ package com.gaurav.avnc.ui.home
 
 import android.view.*
 import androidx.core.view.forEach
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
@@ -29,9 +27,6 @@ import com.gaurav.avnc.viewmodel.HomeViewModel
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  * This class creates and manages tabs in [HomeActivity].
@@ -154,11 +149,6 @@ class ServerTabs(val activity: HomeActivity) {
 
         inner class ViewHolder(val binding: ServerSavedItemBinding)
             : ProfileViewHolder(viewModel, binding.root, R.menu.saved_server)
-
-        object Differ : DiffUtil.ItemCallback<ServerProfile>() {
-            override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old.ID == new.ID)
-            override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
-        }
     }
 
 
@@ -177,25 +167,8 @@ class ServerTabs(val activity: HomeActivity) {
         binding.discoveredRv.setHasFixedSize(true)
 
         activity.viewModel.discovery.servers.observe(activity) { adapter.submitList(it) }
-        launchDiscoveryRestarter()
 
         return binding.root
-    }
-
-    private fun launchDiscoveryRestarter() {
-        activity.lifecycleScope.launch(Dispatchers.Main) {
-            val viewModel = activity.viewModel
-
-            while (true) {
-                delay(viewModel.pref.server.discoveryTimeout + viewModel.pref.server.discoveryRestartDelay)
-
-                if (activity.lifecycle.currentState == Lifecycle.State.RESUMED &&
-                    viewModel.pref.server.discoveryAutoStart &&
-                    viewModel.pref.server.discoveryRestart) {
-                    viewModel.startDiscovery()
-                }
-            }
-        }
     }
 
     class DiscoveredServerAdapter(val viewModel: HomeViewModel)
@@ -219,14 +192,6 @@ class ServerTabs(val activity: HomeActivity) {
             init {
                 binding.saveBtn.setOnClickListener { viewModel.onNewProfile(profile) }
             }
-        }
-
-        /**
-         * Profiles generated by discovery don't have unique IDs, so we compare the whole profile.
-         */
-        object Differ : DiffUtil.ItemCallback<ServerProfile>() {
-            override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
-            override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
         }
     }
 
@@ -272,5 +237,10 @@ class ServerTabs(val activity: HomeActivity) {
             homeViewModel.setClipboardText(text)
             Snackbar.make(rootView, R.string.msg_copied_to_clipboard, Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    private object Differ : DiffUtil.ItemCallback<ServerProfile>() {
+        override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old.ID == new.ID)
+        override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
     }
 }

app/src/main/java/com/gaurav/avnc/vnc/Discovery.kt
@@ -101,11 +101,12 @@ class Discovery(private val context: Context) {
      * Adds a new profile with given details to list.
      */
     private fun addProfile(name: String, host: String, port: Int) {
-        val profile = ServerProfile().apply {
-            this.name = name
-            this.host = host
-            this.port = port
-        }
+        val profile = ServerProfile(
+                ID = (name + host + port).hashCode().toLong(),
+                name = name,
+                host = host,
+                port = port
+        )
 
         runBlocking(Dispatchers.Main) {
             val currentList = servers.value!!
@@ -168,7 +169,6 @@ class Discovery(private val context: Context) {
      */
     private inner class ResolveListener : NsdManager.ResolveListener {
         override fun onServiceResolved(serviceInfo: NsdServiceInfo) {
-            Log.d(javaClass.simpleName, "Resolved service: ${serviceInfo.serviceName.take(3)}*******")
             addProfile(serviceInfo.serviceName, serviceInfo.host.hostAddress, serviceInfo.port)
         }
 

app/src/main/java/com/gaurav/avnc/ui/home/HomeActivity.kt
@@ -59,6 +59,17 @@ class HomeActivity : AppCompatActivity() {
         viewModel.discovery.servers.observe(this) { updateDiscoveryBadge(it) }
     }
 
+    override fun onStart() {
+        super.onStart()
+        viewModel.autoStartDiscovery()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (!isChangingConfigurations)
+            viewModel.autoStopDiscovery()
+    }
+
     /**
      * Handle drawer item selection.
      */

app/src/main/java/com/gaurav/avnc/util/AppPreferences.kt
@@ -15,8 +15,6 @@ import androidx.preference.PreferenceManager
 
 /**
  * Utility class for accessing app preferences
- *
- * - All timeouts are in milliseconds.
  */
 class AppPreferences(context: Context) {
 
@@ -63,13 +61,9 @@ class AppPreferences(context: Context) {
     }
 
     inner class Server {
-        val discoveryTimeout; get() = prefs.getInt("discovery_timeout", 10) * 1000L
-        val discoveryAutoStart; get() = prefs.getBoolean("discovery_auto_start", true)
-        val discoveryRestart; get() = prefs.getBoolean("discovery_restart", false)
-        val discoveryRestartDelay; get() = prefs.getInt("discovery_restart_delay", 5) * 60 * 1000L
-
         val credAutocomplete; get() = prefs.getBoolean("cred_autocomplete", true)
         val clipboardSync; get() = prefs.getBoolean("clipboard_sync", true)
+        val discoveryAutorun; get() = prefs.getBoolean("discovery_autorun", true)
     }
 
     inner class Experimental {

app/src/main/java/com/gaurav/avnc/viewmodel/HomeViewModel.kt
@@ -10,11 +10,8 @@ package com.gaurav.avnc.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.viewModelScope
 import com.gaurav.avnc.model.ServerProfile
 import com.gaurav.avnc.vnc.Discovery
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class HomeViewModel(app: Application) : BaseViewModel(app) {
 
@@ -57,27 +54,37 @@ class HomeViewModel(app: Application) : BaseViewModel(app) {
      */
     val profileDeletedEvent = LiveEvent<ServerProfile>()
 
-
-    init {
-        if (pref.server.discoveryAutoStart)
-            startDiscovery()
-    }
-
     /**
      * Starts new connection to given profile.
      */
     fun startConnection(profile: ServerProfile) = newConnectionEvent.fire(profile)
 
-    /**
-     * Starts discovery service.
-     * It will be automatically stopped after timeout.
-     */
-    fun startDiscovery() {
-        discovery.start()
+    /**************************************************************************
+     * Server Discovery
+     *
+     * To save battery, Discovery is stopped when HomeActivity is in background.
+     **************************************************************************/
+    private var autoStopped = false
 
-        viewModelScope.launch {
-            runCatching { delay(pref.server.discoveryTimeout) }
-            discovery.stop()
+    fun startDiscovery() {
+        autoStopped = false
+        discovery.start()
+    }
+
+    fun stopDiscovery() {
+        autoStopped = false
+        discovery.stop()
+    }
+
+    fun autoStartDiscovery() {
+        if (pref.server.discoveryAutorun || autoStopped)
+            startDiscovery()
+    }
+
+    fun autoStopDiscovery() {
+        if (discovery.isRunning.value == true) {
+            stopDiscovery()
+            autoStopped = true
         }
     }
 

app/src/main/res/layout/server_discovery.xml
@@ -34,7 +34,7 @@
             style="@style/TipView"
             android:text="@string/tip_discovery"
             android:visibility="gone"
-            app:isVisible="@{!viewModel.discovery.isRunning &amp;&amp; viewModel.discovery.servers.empty}"
+            app:isVisible="@{viewModel.discovery.servers.empty}"
             tools:visibility="visible" />
 
         <com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -57,5 +57,15 @@
             android:contentDescription="@string/desc_discovery_running"
             android:indeterminate="true"
             app:isVisible="@{viewModel.discovery.isRunning}" />
+
+        <ImageButton
+            style="@style/ImageButton"
+            android:layout_gravity="end|bottom"
+            android:layout_margin="@dimen/margin_normal"
+            android:contentDescription="@string/desc_discovery_stop_btn"
+            android:onClick="@{()-> viewModel.stopDiscovery()}"
+            android:src="@drawable/ic_clear"
+            app:isVisible="@{viewModel.discovery.isRunning}"
+            app:tint="?colorSecondary" />
     </FrameLayout>
 </layout>

app/src/main/res/values/strings.xml
@@ -80,6 +80,7 @@
     <string name="desc_add_new_server_btn">Add server</string>
     <string name="desc_save_discovered_server_btn">Save</string>
     <string name="desc_discovery_btn">Discover servers</string>
+    <string name="desc_discovery_stop_btn">Stop discovering servers</string>
     <string name="desc_discovery_running">Discovering servers on you network</string>
     <string name="desc_keyboard_btn">Open keyboard</string>
     <string name="desc_virtual_keys_btn">Show virtual keys</string>
@@ -161,12 +162,8 @@
     <string name="pref_cred_autocomplete">Auto-completion</string>
     <string name="pref_cred_autocomplete_summary">Suggest saved logins when connecting to new servers</string>
     <string name="pref_discovery">Discovery</string>
-    <string name="pref_discovery_timeout">Timeout (seconds)</string>
-    <string name="pref_discovery_auto_start">Auto start</string>
-    <string name="pref_discovery_auto_start_summary">Discover servers when opening the app</string>
-    <string name="pref_discovery_restart">Restart</string>
-    <string name="pref_discovery_restart_summary">Restart discovery after it stops</string>
-    <string name="pref_discovery_restart_delay">Restart delay (minutes)</string>
+    <string name="pref_discovery_autorun">Autorun</string>
+    <string name="pref_discovery_autorun_summary">Discover servers while on homepage</string>
     <string name="pref_import_export">Import/Export servers</string>
     <string name="pref_debugging">Debugging</string>
     <string name="pref_logs">Logs</string>

app/src/main/res/xml/pref_server.xml
@@ -6,8 +6,7 @@
   ~ See COPYING.txt for more details.
   -->
 
-<PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
+<PreferenceScreen xmlns:app="http://schemas.android.com/apk/res-auto"
     app:title="@string/pref_servers">
 
 
@@ -32,37 +31,11 @@
         app:icon="@drawable/ic_search"
         app:title="@string/pref_discovery">
 
-        <SeekBarPreference
-            android:max="90"
-            app:defaultValue="10"
-            app:key="discovery_timeout"
-            app:min="5"
-            app:showSeekBarValue="true"
-            app:title="@string/pref_discovery_timeout"
-            app:updatesContinuously="true" />
-
         <SwitchPreference
             app:defaultValue="true"
-            app:key="discovery_auto_start"
-            app:summary="@string/pref_discovery_auto_start_summary"
-            app:title="@string/pref_discovery_auto_start" />
-
-        <SwitchPreference
-            android:dependency="discovery_auto_start"
-            app:defaultValue="false"
-            app:key="discovery_restart"
-            app:summary="@string/pref_discovery_restart_summary"
-            app:title="@string/pref_discovery_restart" />
-
-        <SeekBarPreference
-            android:dependency="discovery_restart"
-            android:max="30"
-            app:defaultValue="5"
-            app:key="discovery_restart_delay"
-            app:min="1"
-            app:showSeekBarValue="true"
-            app:title="@string/pref_discovery_restart_delay"
-            app:updatesContinuously="true" />
+            app:key="discovery_autorun"
+            app:summary="@string/pref_discovery_autorun_summary"
+            app:title="@string/pref_discovery_autorun" />
 
     </PreferenceCategory>
 

commit a518be5a7af8760cff95eb24c955f6889f0577f2

Don't set ID for ServerProfile created during Discovery
A non-zero ID is used to differentiate between saved/non-saved profiles.
(May we should have a better way for that?)

Regression from: 2b64933fcb65ceb375fce46695b255b9005430b1

app/src/main/java/com/gaurav/avnc/ui/home/ServerTabs.kt
@@ -140,6 +140,11 @@ class ServerTabs(val activity: HomeActivity) {
 
         inner class ViewHolder(val binding: ServerSavedItemBinding)
             : ProfileViewHolder(viewModel, binding.root, R.menu.saved_server)
+
+        object Differ : DiffUtil.ItemCallback<ServerProfile>() {
+            override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old.ID == new.ID)
+            override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
+        }
     }
 
 
@@ -183,6 +188,14 @@ class ServerTabs(val activity: HomeActivity) {
             init {
                 binding.saveBtn.setOnClickListener { viewModel.onNewProfile(profile) }
             }
+        }
+
+        /**
+         * Profiles generated by discovery don't have unique IDs, so we compare the whole profile.
+         */
+        object Differ : DiffUtil.ItemCallback<ServerProfile>() {
+            override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
+            override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
         }
     }
 
@@ -228,10 +241,5 @@ class ServerTabs(val activity: HomeActivity) {
             homeViewModel.setClipboardText(text)
             Snackbar.make(rootView, R.string.msg_copied_to_clipboard, Snackbar.LENGTH_SHORT).show()
         }
-    }
-
-    private object Differ : DiffUtil.ItemCallback<ServerProfile>() {
-        override fun areItemsTheSame(old: ServerProfile, new: ServerProfile) = (old.ID == new.ID)
-        override fun areContentsTheSame(old: ServerProfile, new: ServerProfile) = (old == new)
     }
 }

app/src/main/java/com/gaurav/avnc/vnc/Discovery.kt
@@ -102,7 +102,6 @@ class Discovery(private val context: Context) {
      */
     private fun addProfile(name: String, host: String, port: Int) {
         val profile = ServerProfile(
-                ID = (name + host + port).hashCode().toLong(),
                 name = name,
                 host = host,
                 port = port