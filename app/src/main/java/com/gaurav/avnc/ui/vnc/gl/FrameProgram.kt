c6716150256bc9e0d6f14a626282208c04ff032b
Gaurav Ujjwal <gujjwal00@gmail.com>
2022-07-15 23:21:11 +0530

    OpenGL: Set texture wrap mode to GL_CLAMP_TO_EDGE
    In OpenGL ES 2, support for sampling non-power-of-two textures in
    fragment shaders is only guaranteed when wrap mode is GL_CLAMP_TO_EDGE.
    Although most modern GPUs don't enforce this requirement, some devices
    (like Tegra from Nvidia) do.
    
    Related Github Issue: #85

--- app/src/main/java/com/gaurav/avnc/ui/vnc/gl/FrameProgram.kt
+++ app/src/main/java/com/gaurav/avnc/ui/vnc/gl/FrameProgram.kt
@@ -56,6 +56,8 @@ class FrameProgram {
         glBindTexture(GL_TEXTURE_2D, texturesObjects[0])
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
         glBindTexture(GL_TEXTURE_2D, 0)
         return texturesObjects[0]
     }
