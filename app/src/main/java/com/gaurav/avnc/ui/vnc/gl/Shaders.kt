1af619551a04c875d7140043b7702fb5504f014f
Gaurav Ujjwal <gujjwal00@gmail.com>
2022-07-15 23:12:02 +0530

    OpenGL: Use correct type for position attribute in Vertex Shader
    For a_Position attribute, Frame.bind() only assign the x,y components,
    but a_Position was declared as vec4.
    
    While this generally does not cause any issue, its much better to
    use the correct data type to avoid any possible misbehavior.

--- app/src/main/java/com/gaurav/avnc/ui/vnc/gl/Shaders.kt
+++ app/src/main/java/com/gaurav/avnc/ui/vnc/gl/Shaders.kt
@@ -15,13 +15,13 @@ object Shaders {
     //language=GLSL
     const val VERTEX_SHADER = """
             uniform mat4 u_Projection;
-            attribute vec4 a_Position;
+            attribute vec2 a_Position;
             attribute vec2 a_TextureCoordinates;
             varying vec2 v_TextureCoordinates;
             void main()
             {
                v_TextureCoordinates = a_TextureCoordinates;
-               gl_Position = u_Projection * a_Position;
+               gl_Position = u_Projection * vec4(a_Position, 0, 1);
             }"""
 
     //language=GLSL
