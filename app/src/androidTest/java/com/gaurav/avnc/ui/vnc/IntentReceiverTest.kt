a52baca9cd3be70ec1f977482316877effab5695
Gaurav Ujjwal <gujjwal00@gmail.com>
2025-06-13 16:52:32 +0530

    Add support for 'SaveConnection' URI parameter

--- app/src/androidTest/java/com/gaurav/avnc/ui/vnc/IntentReceiverTest.kt
+++ app/src/androidTest/java/com/gaurav/avnc/ui/vnc/IntentReceiverTest.kt
@@ -11,9 +11,14 @@ package com.gaurav.avnc.ui.vnc
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.SystemClock
+import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withSubstring
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -21,11 +26,14 @@ import androidx.test.filters.SdkSuppress
 import com.gaurav.avnc.EmptyDatabaseRule
 import com.gaurav.avnc.R
 import com.gaurav.avnc.TestServer
+import com.gaurav.avnc.checkIsDisplayed
 import com.gaurav.avnc.checkWillBeDisplayed
 import com.gaurav.avnc.model.ServerProfile
 import com.gaurav.avnc.onToast
+import com.gaurav.avnc.pollingAssert
 import com.gaurav.avnc.targetContext
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -39,6 +47,27 @@ class IntentReceiverTest {
     @Rule
     @JvmField
     val dbRule = EmptyDatabaseRule()
+
+    // Once a toast is matched we need to wait for it to be closed so that
+    // the the toast from the next test is displayed and matched properly
+    class WaitForViewDetach : ViewAction {
+        override fun getDescription() = "waiting for the view to detach from window"
+        override fun getConstraints() = isDisplayed()!!
+        override fun perform(uiController: UiController, view: View) {
+            val timeout = SystemClock.uptimeMillis() + 5000
+            do {
+                if (!view.isAttachedToWindow)
+                    return
+                uiController.loopMainThreadForAtLeast(100)
+            } while (SystemClock.uptimeMillis() < timeout)
+
+            throw Exception("Timeout expired waiting for the view to detach from window")
+        }
+    }
+
+    private fun assertToastIsShown(matcher: Matcher<View>) {
+        pollingAssert { onToast(matcher).checkIsDisplayed().perform(WaitForViewDetach()) }
+    }
 
     /******************************** VNC URIs ***************************************/
 
@@ -61,7 +90,7 @@ class IntentReceiverTest {
     @SdkSuppress(maxSdkVersion = 29)
     fun invalidVncUri() {
         ActivityScenario.launch<Activity>(newUriIntent("vnc://:5900"))
-        onToast(withText(R.string.msg_invalid_vnc_uri)).checkWillBeDisplayed()
+        assertToastIsShown(withText(R.string.msg_invalid_vnc_uri))
     }
 
     @Test
@@ -79,7 +108,54 @@ class IntentReceiverTest {
     @SdkSuppress(maxSdkVersion = 29)
     fun uriWithUnknownConnectionName() {
         ActivityScenario.launch<Activity>(newUriIntent("vnc://?ConnectionName=NoSuchServer"))
-        onToast(withSubstring("No server found with name")).checkWillBeDisplayed()
+        assertToastIsShown(withSubstring("No server found with name"))
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = 29)
+    fun saveUri() {
+        ActivityScenario.launch<Activity>(newUriIntent("vnc://localhost?ConnectionName=Test&SaveConnection=true"))
+        assertToastIsShown(withSubstring("Added new server for URI"))
+
+        val profiles = runBlocking { dbRule.db.serverProfileDao.getList() }
+        assertEquals(1, profiles.size)
+        assertEquals("Test", profiles.first().name)
+        assertEquals("localhost", profiles.first().host)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = 29)
+    fun saveUriWithoutExplicitName() {
+        ActivityScenario.launch<Activity>(newUriIntent("vnc://localhost/?SaveConnection=true"))
+        assertToastIsShown(withSubstring("Added new server for URI"))
+
+        val profiles = runBlocking { dbRule.db.serverProfileDao.getList() }
+        assertEquals(1, profiles.size)
+        assertEquals("vnc://localhost", profiles.first().name) // Name should be generated using host
+        assertEquals("localhost", profiles.first().host)
+    }
+
+    @Test
+    fun updateProfileWithUri() {
+        runBlocking { dbRule.db.serverProfileDao.save(ServerProfile(name = "Test", host = "10.0.0.1").apply { viewOnly = true }) }
+        ActivityScenario.launch<Activity>(newUriIntent("vnc://localhost?SaveConnection=true&ConnectionName=Test&ViewOnly=false"))
+                .use { }
+
+        val profiles = runBlocking { dbRule.db.serverProfileDao.getList() }
+        assertEquals(1, profiles.size)
+        assertEquals("Test", profiles.first().name)
+        assertEquals("localhost", profiles.first().host)
+        assertEquals(false, profiles.first().viewOnly)
+    }
+
+    @Test
+    @SdkSuppress(maxSdkVersion = 29)
+    fun tryToSaveInvalidUri() {
+        ActivityScenario.launch<Activity>(newUriIntent("vnc://?SaveConnection=true&ConnectionName=Test"))
+        assertToastIsShown(withSubstring("No server found with name"))
+
+        val profiles = runBlocking { dbRule.db.serverProfileDao.getList() }
+        assertEquals(0, profiles.size)
     }
 
 
@@ -102,6 +178,6 @@ class IntentReceiverTest {
     @SdkSuppress(maxSdkVersion = 29)
     fun invalidShortcut() {
         ActivityScenario.launch<Activity>(newShortcutIntent(ServerProfile(ID = 123456)))
-        onToast(withText(R.string.msg_shortcut_server_deleted)).checkWillBeDisplayed()
+        assertToastIsShown(withText(R.string.msg_shortcut_server_deleted))
     }
 }
--- app/src/main/java/com/gaurav/avnc/ui/vnc/IntentReceiverActivity.kt
+++ app/src/main/java/com/gaurav/avnc/ui/vnc/IntentReceiverActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.gaurav.avnc.R
+import com.gaurav.avnc.model.ServerProfile
 import com.gaurav.avnc.model.db.MainDb
 import com.gaurav.avnc.vnc.VncUri
 import kotlinx.coroutines.launch
@@ -48,7 +49,7 @@ class IntentReceiverActivity : AppCompatActivity() {
 
     private fun handleIntent() = lifecycleScope.launch {
         if (intent.data?.scheme == "vnc")
-            launchFromVncUri(VncUri(intent.data!!.toString()))
+            handleVncUri(VncUri(intent.data!!.toString()))
         else if (intent.hasExtra(SHORTCUT_PROFILE_ID_KEY))
             launchFromProfileId(intent.getLongExtra(SHORTCUT_PROFILE_ID_KEY, 0))
         else
@@ -57,26 +58,88 @@ class IntentReceiverActivity : AppCompatActivity() {
         finish()
     }
 
+    private suspend fun handleVncUri(uri: VncUri) {
+        if (!validateUri(uri))
+            return
+
+        if (uri.saveConnection)
+            saveVncUri(uri)
+
+        launchFromVncUri(uri)
+    }
+
     private suspend fun launchFromVncUri(uri: VncUri) {
-        if (uri.connectionName.isNotBlank()) launchFromProfileName(uri.connectionName)
-        else launchVncUri(uri)
-    }
+        val matchingProfile = findAndMergeMatchingProfile(uri)
 
-    private fun launchVncUri(uri: VncUri) {
-        if (uri.host.isEmpty()) toast(getString(R.string.msg_invalid_vnc_uri))
-        else startVncActivity(this, uri)
-    }
-
-    private suspend fun launchFromProfileName(name: String) {
-        val profile = profileDao.getByName(name).firstOrNull()
-        if (profile == null) toast("No server found with name '$name'")
-        else startVncActivity(this, profile)
+        if (matchingProfile != null)
+            startVncActivity(this, matchingProfile)
+        else if (!uri.host.isNullOrBlank())
+            startVncActivity(this, uri)
+        else if (!uri.connectionName.isNullOrBlank())
+            toast("No server found with name '${uri.connectionName}'")
+        else
+            toast(getString(R.string.msg_invalid_vnc_uri))
     }
 
     private suspend fun launchFromProfileId(profileId: Long) {
         val profile = profileDao.getByID(profileId)
         if (profile == null) toast(getString(R.string.msg_shortcut_server_deleted))
         else startVncActivity(this, profile)
+    }
+
+
+    private suspend fun findAndMergeMatchingProfile(uri: VncUri): ServerProfile? {
+        return uri.connectionNameForProfile
+                ?.let { profileDao.getByName(it).firstOrNull() }
+                ?.let { uri.applyToProfile(it) }
+    }
+
+    private suspend fun saveVncUri(uri: VncUri) {
+        if (!uri.saveConnection)
+            return
+
+        val profile = findAndMergeMatchingProfile(uri) ?: uri.toServerProfile()
+        if (profile.host.isBlank())
+            return
+
+        val id = profileDao.save(profile)
+        if (id > 0) // New profile
+            toast("Added new server for URI")
+    }
+
+    private fun validateUri(uri: VncUri): Boolean {
+        if (uri.host.isNullOrBlank() && uri.connectionName.isNullOrBlank()) {
+            toast(getString(R.string.msg_invalid_vnc_uri))
+            return false
+        }
+
+        if (uri.host.isNullOrBlank() && uri.connectionName.isNullOrBlank()) {
+            toast(getString(R.string.msg_invalid_vnc_uri))
+            return false
+        }
+
+        if (uri.channelType !in listOf(null, ServerProfile.CHANNEL_TCP, ServerProfile.CHANNEL_SSH_TUNNEL)) {
+            toast("Unknown channel type: ${uri.channelType}")
+            return false
+        }
+
+        val supportedSecurityTypes = resources.getStringArray(R.array.profile_editor_security_values)
+        if (uri.securityType != null && uri.securityType.toString() !in supportedSecurityTypes) {
+            toast("Unknown security type: ${uri.securityType}")
+            return false
+        }
+
+        if (uri.channelType == ServerProfile.CHANNEL_SSH_TUNNEL && uri.sshHost.isNullOrBlank()) {
+            toast("Missing SSH host in URI")
+            return false
+        }
+
+        if (uri.channelType == ServerProfile.CHANNEL_SSH_TUNNEL && uri.sshUsername.isNullOrBlank()) {
+            toast("Missing SSH username in URI")
+            return false
+        }
+
+        return true
     }
 
     private fun toast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
