c4bd73fc93958e34cd24c8400d8203c01d34f049
Gaurav Ujjwal <gujjwal00@gmail.com>
2026-02-04 14:44:19 +0530

    Refactor FrameState: Directly use synchronised methods instead of via a lock

--- app/src/main/java/com/gaurav/avnc/ui/vnc/FrameState.kt
+++ app/src/main/java/com/gaurav/avnc/ui/vnc/FrameState.kt
@@ -153,30 +153,31 @@ class FrameState(
             val scale: Float
     )
 
-    private val lock = Any()
-    private inline fun <T> withLock(block: () -> T) = synchronized(lock) { block() }
-
-    fun setFramebufferSize(w: Float, h: Float) = withLock {
+    @Synchronized
+    fun setFramebufferSize(w: Float, h: Float) {
         fbWidth = w
         fbHeight = h
         calculateBaseScale()
         coerceValues()
     }
 
-    fun setViewportSize(w: Float, h: Float) = withLock {
+    @Synchronized
+    fun setViewportSize(w: Float, h: Float) {
         vpWidth = w
         vpHeight = h
         coerceValues()
     }
 
-    fun setWindowSize(w: Float, h: Float) = withLock {
+    @Synchronized
+    fun setWindowSize(w: Float, h: Float) {
         windowWidth = w
         windowHeight = h
         calculateBaseScale()
         coerceValues()
     }
 
-    fun setSafeArea(rect: RectF) = withLock {
+    @Synchronized
+    fun setSafeArea(rect: RectF) {
         safeArea = RectF(rect)
         coerceValues()
     }
@@ -186,7 +187,8 @@ class FrameState(
      *
      * Returns 'how much' scale factor is actually applied (after coercing).
      */
-    fun updateZoom(scaleFactor: Float): Float = withLock {
+    @Synchronized
+    fun updateZoom(scaleFactor: Float): Float {
         val oldScale = zoomScale
         val newScale = zoomScale * scaleFactor
 
@@ -196,7 +198,8 @@ class FrameState(
         return zoomScale / oldScale //Applied scale factor
     }
 
-    fun setZoom(zoom1: Float, zoom2: Float) = withLock {
+    @Synchronized
+    fun setZoom(zoom1: Float, zoom2: Float) {
         zoomScale1 = zoom1
         zoomScale2 = zoom2
         coerceValues()
@@ -205,7 +208,8 @@ class FrameState(
     /**
      * Shift frame by given delta.
      */
-    fun pan(deltaX: Float, deltaY: Float) = withLock {
+    @Synchronized
+    fun pan(deltaX: Float, deltaY: Float) {
         frameX += deltaX
         frameY += deltaY
         coerceValues()
@@ -214,7 +218,8 @@ class FrameState(
     /**
      * Move frame to given position.
      */
-    fun moveTo(x: Float, y: Float) = withLock {
+    @Synchronized
+    fun moveTo(x: Float, y: Float) {
         frameX = x
         frameY = y
         coerceValues()
@@ -225,6 +230,7 @@ class FrameState(
      * Not all viewport points have a corresponding framebuffer point
      * because drawn frame can be smaller than viewport.
      */
+    @Synchronized
     fun toFbUnchecked(vpPoint: PointF): PointF {
         val fbX = (vpPoint.x - frameX) / scale
         val fbY = (vpPoint.y - frameY) / scale
@@ -235,6 +241,7 @@ class FrameState(
      * Converts given viewport point to corresponding framebuffer point.
      * Returns null if given point lies outside of framebuffer.
      */
+    @Synchronized
     fun toFb(vpPoint: PointF): PointF? {
         val fb = toFbUnchecked(vpPoint)
         if (fb.x < 0F || fb.y < 0f || fb.x >= fbWidth || fb.y >= fbHeight)
@@ -246,6 +253,7 @@ class FrameState(
     /**
      * Converts given framebuffer point to corresponding point in viewport.
      */
+    @Synchronized
     fun toVP(fbPoint: PointF): PointF {
         return PointF(fbPoint.x * scale + frameX, fbPoint.y * scale + frameY)
     }
@@ -253,7 +261,8 @@ class FrameState(
     /**
      * Returns immutable & consistent snapshot of frame state.
      */
-    fun getSnapshot(): Snapshot = withLock {
+    @Synchronized
+    fun getSnapshot(): Snapshot {
         return Snapshot(frameX = frameX, frameY = frameY,
                         fbWidth = fbWidth, fbHeight = fbHeight,
                         vpWidth = vpWidth, vpHeight = vpHeight, scale = scale)
