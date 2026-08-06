cbff6ed9bf2dc6d50ad848865587de55edeeb57b
Gaurav Ujjwal <gujjwal00@gmail.com>
2026-08-06 19:23:09 +0530

    Reduce unnecessary differences with master branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
 
